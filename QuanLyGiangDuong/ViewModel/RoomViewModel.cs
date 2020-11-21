236b04ad39dc389f98dd5d80e9ce3d4cd44351d9
hcthcm290 <dragonnica123@gmail.com>
2020-11-21 17:22:12 +0700

    Load UsingEvent into RoomSchedule

--- QuanLyGiangDuong/ViewModel/RoomViewModel.cs
+++ QuanLyGiangDuong/ViewModel/RoomViewModel.cs
@@ -2,13 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QuanLyGiangDuong.ViewModel
 {
-    public class RoomViewModel
+    public class RoomViewModel: BaseViewModel
     {
         public class RoomTableRow
         {
@@ -184,6 +186,186 @@ namespace QuanLyGiangDuong.ViewModel
             }
         }
 
+        private DateTime _startDate;
+        public DateTime startDate
+        {
+            get
+            {
+                return _startDate;
+            }
+
+            set
+            {
+                _startDate = value;
+                OnPropertyChanged("weekTitle");
+            }
+        }
+
+        private DateTime _endDate;
+        public DateTime endDate
+        {
+            get
+            {
+                return _endDate;
+            }
+
+            set
+            {
+                _endDate = value;
+                OnPropertyChanged("weekTitle");
+            }
+        }
+
+        private string _weekTitle;
+        public string weekTitle
+        {
+            get
+            {
+                _weekTitle = _startDate.ToShortDateString() + " - " + _endDate.ToShortDateString();
+                return _weekTitle;
+            }
+
+            set
+            {
+                _weekTitle = value;
+            }
+        }
+
+        private ICommand _getRoomScheduleCommand;
+        public ICommand GetRoomScheduleCommand
+        {
+            get
+            {
+                if (_getRoomScheduleCommand == null)
+                {
+                    _getRoomScheduleCommand = new RelayCommand(
+                        param => this.GetRoomSchedule()
+                    );
+                }
+                return _getRoomScheduleCommand;
+            }
+        }
+
+        public void GetRoomSchedule()
+        {
+            startDate = new DateTime(selectedYear, selectedMonth.monthValue, selectedDay);
+            startDate = startDate.AddDays(-((int)startDate.DayOfWeek + 6)%7);
+            endDate = startDate.AddDays(6);
+
+            listRoomTableRow.Clear();
+
+            GetUsingClass();
+            GetUsingEvent();
+        }
+
+        public void GetUsingClass()
+        {
+            var usingClass = (from uc in DataProvider.Ins.DB.USINGCLASSes
+                              join cl in DataProvider.Ins.DB.CLASSes on uc.ClassID equals cl.ClassID
+                              join rm in DataProvider.Ins.DB.ROOMs on uc.RoomID equals rm.RoomID
+                              where DbFunctions.AddDays(_startDate, (uc.Day_ + 6) % 7) > cl.StartDate &&   // Satisfy the start and end date of class
+                                    DbFunctions.AddDays(_startDate, (uc.Day_ + 6) % 7) < cl.EndDate &&      // Satisfy the start and end date of class
+                                    (DbFunctions.DiffDays(DbFunctions.AddDays(_startDate, (uc.Day_ + 6) % 7), cl.StartDate) / 7) % uc.RepeatCycle == 0 // satisfied the repeat cycle
+                              select new { uc, cl, rm }
+                        ).ToList();
+
+            foreach (var ins in usingClass)
+            {
+                if (!IsExistRoom(ins.rm))
+                {
+                    var listPeriod = (from period in DataProvider.Ins.DB.PERIOD_TIMERANGE
+                                      where period.PeriodID != -1
+                                      select period).ToList();
+                    foreach (var period in listPeriod)
+                    {
+                        RoomTableRow roomTableRow = new RoomTableRow();
+                        roomTableRow.room = ins.rm;
+                        roomTableRow.period = period;
+
+                        listRoomTableRow.Add(roomTableRow);
+                    }
+                }
+
+                // for each using, we split it into multiple period row
+                var listPeriodTimeRange = Utilities.Utils.GetListPeriodTimeRange(ins.uc.StartPeriod, ins.uc.Duration);
+
+                foreach (var periodTimeRange in listPeriodTimeRange)
+                {
+                    if (IsExistRoomRow(ins.rm, periodTimeRange))
+                    {
+                        var roomTableRow = listRoomTableRow.First((x) => {
+                            return x.room.RoomID == ins.rm.RoomID &&
+                                   x.period.PeriodID == periodTimeRange.PeriodID;
+                        });
+
+                        roomTableRow.usings[ins.uc.Day_] = ins.uc;
+                    }
+                    else
+                    {
+                        RoomTableRow roomTableRow = new RoomTableRow();
+                        roomTableRow.room = ins.rm;
+                        roomTableRow.period = periodTimeRange;
+                        roomTableRow.usings[ins.uc.Day_] = ins.uc;
+
+                        listRoomTableRow.Add(roomTableRow);
+                    }
+                }
+            }
+        }
+        public void GetUsingEvent()
+        {
+            var usingEvent = (from ev in DataProvider.Ins.DB.USINGEVENTs
+                              join cl in DataProvider.Ins.DB.EVENT_ on ev.EventID equals cl.EventID
+                              join rm in DataProvider.Ins.DB.ROOMs on ev.RoomID equals rm.RoomID
+                              where ev.Date_ > startDate &&   // Satisfy the start and end date of choosen week
+                                    ev.Date_ < endDate     // Satisfy the start and end date of choosen week
+                              select new { ev, cl, rm }
+                        ).ToList();
+
+            foreach (var ins in usingEvent)
+            {
+                if (!IsExistRoom(ins.rm))
+                {
+                    var listPeriod = (from period in DataProvider.Ins.DB.PERIOD_TIMERANGE
+                                      where period.PeriodID != -1
+                                      select period).ToList();
+                    foreach (var period in listPeriod)
+                    {
+                        RoomTableRow roomTableRow = new RoomTableRow();
+                        roomTableRow.room = ins.rm;
+                        roomTableRow.period = period;
+
+                        listRoomTableRow.Add(roomTableRow);
+                    }
+                }
+
+                // for each using, we split it into multiple period row
+                var listPeriodTimeRange = Utilities.Utils.GetListPeriodTimeRange(ins.ev.StartPeriod, ins.ev.Duration);
+
+                foreach (var periodTimeRange in listPeriodTimeRange)
+                {
+                    if (IsExistRoomRow(ins.rm, periodTimeRange))
+                    {
+                        var roomTableRow = listRoomTableRow.First((x) => {
+                            return x.room.RoomID == ins.rm.RoomID &&
+                                   x.period.PeriodID == periodTimeRange.PeriodID;
+                        });
+
+                        roomTableRow.usings[(int)ins.ev.Date_.DayOfWeek] = ins.ev;
+                    }
+                    else
+                    {
+                        RoomTableRow roomTableRow = new RoomTableRow();
+                        roomTableRow.room = ins.rm;
+                        roomTableRow.period = periodTimeRange;
+                        roomTableRow.usings[(int)ins.ev.Date_.DayOfWeek] = ins.ev;
+
+                        listRoomTableRow.Add(roomTableRow);
+                    }
+                }
+            }
+        }
+
         bool IsExistRoomRow(ROOM rm, PERIOD_TIMERANGE period)
         {
             foreach(var roomTableRow in listRoomTableRow)
@@ -210,55 +392,10 @@ namespace QuanLyGiangDuong.ViewModel
 
         public RoomViewModel()
         {
+            // Init startDate and endDate
+            _startDate = DateTime.Now.AddDays(-((int)DateTime.Now.DayOfWeek - 1)); // get the start of the week
+            _endDate = _startDate.AddDays(6); // get the end of the week
 
-            listRoomTableRow.Clear();
-
-            var data = (from uc in DataProvider.Ins.DB.USINGCLASSes
-                        join cl in DataProvider.Ins.DB.CLASSes on uc.ClassID equals cl.ClassID
-                        join rm in DataProvider.Ins.DB.ROOMs on uc.RoomID equals rm.RoomID
-                        select new { uc, cl, rm }
-                        ).ToList();
-
-            foreach(var ins in data)
-            {
-                if(!IsExistRoom(ins.rm))
-                {
-                    var listPeriod = (from period in DataProvider.Ins.DB.PERIOD_TIMERANGE
-                                      where period.PeriodID != -1
-                                      select period).ToList();
-                    foreach(var period in listPeriod)
-                    {
-                        RoomTableRow roomTableRow = new RoomTableRow();
-                        roomTableRow.room = ins.rm;
-                        roomTableRow.period = period;
-
-                        listRoomTableRow.Add(roomTableRow);
-                    }
-                }
-
-                // for each using, we split it into multiple period row
-                var listPeriodTimeRange = Utilities.Utils.GetListPeriodTimeRange(ins.uc.StartPeriod, ins.uc.Duration);
-
-                foreach (var periodTimeRange in listPeriodTimeRange)
-                {
-                    if (IsExistRoomRow(ins.rm, periodTimeRange))
-                    {
-                        var roomTableRow = listRoomTableRow.First((x) => { return x.room.RoomID == ins.rm.RoomID && 
-                                                                                  x.period.PeriodID == periodTimeRange.PeriodID; });
-
-                        roomTableRow.usings[ins.uc.Day_] = ins.uc;
-                    }
-                    else
-                    {
-                        RoomTableRow roomTableRow = new RoomTableRow();
-                        roomTableRow.room = ins.rm;
-                        roomTableRow.period = periodTimeRange;
-                        roomTableRow.usings[ins.uc.Day_] = ins.uc;
-
-                        listRoomTableRow.Add(roomTableRow);
-                    }
-                }
-            }
         }
     }
 }
