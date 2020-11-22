bd91e55f3c8cecc7abb5380698112a576b6427f0
LaNeitD18 <phanhuytien14@gmail.com>
2020-11-22 15:06:18 +0700

    Add colors(primary & accent brown) Add edit & add function in ExamInputViewModel

--- QuanLyGiangDuong/ViewModel/ExamInputViewModel.cs
+++ QuanLyGiangDuong/ViewModel/ExamInputViewModel.cs
@@ -6,52 +6,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace QuanLyGiangDuong.ViewModel
 {
     class ExamInputViewModel : BaseViewModel
     {
         #region Variables
-        private ObservableCollection<TRAINING_PROGRAM> _ListTrainingProgram;
-        public ObservableCollection<TRAINING_PROGRAM> ListTrainingProgram
+        private ObservableCollection<ROOM> _ListRoom;
+        public ObservableCollection<ROOM> ListRoom
         {
-            get { return _ListTrainingProgram; }
-            set { _ListTrainingProgram = value; OnPropertyChanged(); }
+            get { return _ListRoom; }
+            set { _ListRoom = value; OnPropertyChanged(); }
         }
 
-        private TRAINING_PROGRAM _SelectedTrainingProgram;
-        public TRAINING_PROGRAM SelectedTrainingProgram
+        private ROOM _SelectedRoom;
+        public ROOM SelectedRoom
         {
-            get { return _SelectedTrainingProgram; }
-            set { _SelectedTrainingProgram = value; OnPropertyChanged(); }
+            get { return _SelectedRoom; }
+            set { _SelectedRoom = value; OnPropertyChanged(); }
         }
 
-        private ObservableCollection<FACAULTY> _ListFacaulty;
-        public ObservableCollection<FACAULTY> ListFacaulty
+        private ObservableCollection<CLASS> _ListClass;
+        public ObservableCollection<CLASS> ListClass
         {
-            get { return _ListFacaulty; }
-            set { _ListFacaulty = value; OnPropertyChanged(); }
+            get { return _ListClass; }
+            set { _ListClass = value; OnPropertyChanged(); }
         }
 
-        private FACAULTY _SelectedFacaulty;
-        public FACAULTY SelectedFacaulty
+        private CLASS _SelectedClass;
+        public CLASS SelectedClass
         {
-            get { return _SelectedFacaulty; }
-            set { _SelectedFacaulty = value; OnPropertyChanged(); }
+            get { return _SelectedClass; }
+            set { _SelectedClass = value; OnPropertyChanged(); }
         }
 
-        private ObservableCollection<string> _ListSemester;
-        public ObservableCollection<string> ListSemester
+        private ObservableCollection<USINGEXAM> _ListUsingExam;
+        public ObservableCollection<USINGEXAM> ListUsingExam
         {
-            get { return _ListSemester; }
-            set { _ListSemester = value; OnPropertyChanged(); }
+            get { return _ListUsingExam; }
+            set { _ListUsingExam = value; OnPropertyChanged(); }
         }
 
-        private ObservableCollection<int> _ListYear;
-        public ObservableCollection<int> ListYear
+        private USINGEXAM _SelectedUsingExam;
+        public USINGEXAM SelectedUsingExam
         {
-            get { return _ListYear; }
-            set { _ListYear = value; OnPropertyChanged(); }
+            get { return _SelectedUsingExam; }
+            set { _SelectedUsingExam = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<EXAM> _ListExam;
+        public ObservableCollection<EXAM> ListExam
+        {
+            get { return _ListExam; }
+            set { _ListExam = value; OnPropertyChanged(); }
+        }
+
+        private EXAM _SelectedExam;
+        public EXAM SelectedExam
+        {
+            get { return _SelectedExam; }
+            set { _SelectedExam = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<LECTURER> _ListSupervisor;
+        public ObservableCollection<LECTURER> ListSupervisor
+        {
+            get { return _ListSupervisor; }
+            set { _ListSupervisor = value; OnPropertyChanged(); }
+        }
+
+        private LECTURER _SelectedSupervisor;
+        public LECTURER SelectedSupervisor
+        {
+            get { return _SelectedSupervisor; }
+            set { _SelectedSupervisor = value; OnPropertyChanged(); }
         }
 
         private string _ExamSubject;
@@ -59,6 +89,20 @@ namespace QuanLyGiangDuong.ViewModel
         {
             get { return _ExamSubject; }
             set { _ExamSubject = value; OnPropertyChanged(); }
+        }
+
+        private string _UsingExamID;
+        public string UsingExamID
+        {
+            get { return _UsingExamID; }
+            set { _UsingExamID = value; OnPropertyChanged(); }
+        }
+
+        private string _ExamID;
+        public string ExamID
+        {
+            get { return _ExamID; }
+            set { _ExamID = value; OnPropertyChanged(); }
         }
 
         private string _Supervisor;
@@ -102,39 +146,412 @@ namespace QuanLyGiangDuong.ViewModel
             get { return _EndDate; }
             set { _EndDate = value; OnPropertyChanged(); }
         }
+
+        private bool _IsAddingEnabled;
+        public bool IsAddingEnabled
+        {
+            get { return _IsAddingEnabled; }
+            set { _IsAddingEnabled = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsEditingEnabled;
+        public bool IsEditingEnabled
+        {
+            get { return _IsEditingEnabled; }
+            set { _IsEditingEnabled = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsDeletingEnabled;
+        public bool IsDeletingEnabled
+        {
+            get { return _IsDeletingEnabled; }
+            set { _IsDeletingEnabled = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsApprovingEnabled;
+        public bool IsApprovingEnabled
+        {
+            get { return _IsApprovingEnabled; }
+            set { _IsApprovingEnabled = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsRejectingEnabled;
+        public bool IsRejectingEnabled
+        {
+            get { return _IsRejectingEnabled; }
+            set { _IsRejectingEnabled = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsBeingInTask;
+        public bool IsBeingInTask
+        {
+            get { return _IsBeingInTask; }
+            set { _IsBeingInTask = value; OnPropertyChanged(); }
+        }
+
+        private bool _IsEnabledElements;
+        public bool IsEnabledElements
+        {
+            get { return _IsEnabledElements; }
+            set { _IsEnabledElements = value; OnPropertyChanged(); }
+        }
         #endregion
 
         #region Function
         private void LoadData()
         {
-            var listTrainingProgram = DataProvider.Ins.DB.TRAINING_PROGRAM;
-            ListTrainingProgram = new ObservableCollection<TRAINING_PROGRAM>(listTrainingProgram);
+            var listRoom = DataProvider.Ins.DB.ROOMs;
+            ListRoom = new ObservableCollection<ROOM>(listRoom);
 
-            var listFacaulty = DataProvider.Ins.DB.FACAULTies;
-            ListFacaulty = new ObservableCollection<FACAULTY>(listFacaulty);
+            var listClass = DataProvider.Ins.DB.CLASSes;
+            ListClass = new ObservableCollection<CLASS>(listClass);
 
-            ListSemester = new ObservableCollection<string>();
-            ListSemester.Add("Học kỳ I");
-            ListSemester.Add("Học kỳ II");
-            ListSemester.Add("Học kỳ hè");
+            var listUsingExam = DataProvider.Ins.DB.USINGEXAMs;
+            ListUsingExam = new ObservableCollection<USINGEXAM>(listUsingExam);
 
-            ListYear = new ObservableCollection<int>();
-            for(int i=2000; i<=DateTime.Now.Year; i++)
-            {
-                ListYear.Add(i);
+            var listSupervisor = DataProvider.Ins.DB.LECTURERs;
+            ListSupervisor = new ObservableCollection<LECTURER>(listSupervisor);
+        }
+        private void DisableButtons()
+        {
+            IsAddingEnabled = false;
+            IsEditingEnabled = false;
+            IsDeletingEnabled = false;
+            IsApprovingEnabled = false;
+            IsRejectingEnabled = false;
+            IsBeingInTask = false;
+        }
+        private void DisableElements() {
+            IsEnabledElements = false;
+        }
+        private void EnableElements()
+        {
+            IsEnabledElements = true;
+        }
+        private void EnableOnlyAdding()
+        {
+            DisableButtons();
+            IsAddingEnabled = true;
+            IsBeingInTask = true;
+        }
+        private void EnableOnlyEditing()
+        {
+            DisableButtons();
+            IsEditingEnabled = true;
+            IsBeingInTask = true;
+        }
+        private void EnableOnlyDeleting()
+        {
+            DisableButtons();
+            IsDeletingEnabled = true;
+            IsBeingInTask = true;
+        }
+        private void EnableOnlyApproving()
+        {
+            DisableButtons();
+            IsApprovingEnabled = true;
+            IsBeingInTask = true;
+        }
+        private void EnableOnlyRejecting()
+        {
+            DisableButtons();
+            IsRejectingEnabled = true;
+            IsBeingInTask = true;
+        }
+        private void ResetElements()
+        {
+            SelectedRoom = null;
+            SelectedClass = null;
+            SelectedSupervisor = null;
+            StartDate = null;
+            ExamTime = "";
+            Population = "";
+            Description = "";
+        }
+        private void ResetAll()
+        {
+            ResetElements();
+            DisableElements();
+            EnableOnlyAdding();
+            IsBeingInTask = false;
+
+            SelectedUsingExam = null;
+            SelectedExam = null;
+            UsingExamID = "";
+        }
+
+        private void SetValueForElements()
+        {
+            SelectedClass = ListClass.Where(x => x.ClassID == SelectedExam.ClassID).FirstOrDefault();
+            SelectedRoom = ListRoom.Where(x => x.RoomID == SelectedUsingExam.RoomID).FirstOrDefault();
+            SelectedSupervisor = ListSupervisor.Where(x => x.LecturerID == SelectedExam.LecturerID).FirstOrDefault();
+            Population = SelectedExam.Population_.ToString();
+            StartDate = SelectedUsingExam.Date_.ToString();
+            ExamTime = SelectedUsingExam.Duration.Minutes.ToString();
+            Description = SelectedUsingExam.Description_;
+        }
+
+        private string AutoGenerateExamID() {
+            string result = "";
+
+            ObservableCollection<EXAM> listExam = new ObservableCollection<EXAM>(DataProvider.Ins.DB.EXAMs);
+            if(listExam.Count() == 0) {
+                result = "1";
+            } else {
+                result = (listExam.Count() + 1).ToString();
             }
+
+            return result;
+        }
+
+        private string AutoGenerateUsingExamID()
+        {
+            string result = "";
+
+            ObservableCollection<USINGEXAM> listUsingExam = new ObservableCollection<USINGEXAM>(DataProvider.Ins.DB.USINGEXAMs);
+            if (listUsingExam.Count() == 0) {
+                result = "1";
+            } else {
+                result = (listUsingExam.Count() + 1).ToString();
+            }
+
+            return result;
+        }
+
+        private void AddExam()
+        {
+            ExamID = AutoGenerateExamID();
+
+            // add exam to database
+            var exam = new EXAM()
+            {
+                ExamID = ExamID,
+                LecturerID = Supervisor,
+                ClassID = SelectedClass.ClassID,
+                Population_ = Convert.ToInt32(Population)
+            };
+            DataProvider.Ins.DB.EXAMs.Add(exam);
+            DataProvider.Ins.DB.SaveChanges();
+        }
+
+        private void AddUsingExam() {
+            var usingExam = new USINGEXAM() {
+                UsingExamID = UsingExamID,
+                RoomID = SelectedRoom.RoomID,
+                ExamID = ExamID,
+                Date_ = Convert.ToDateTime(StartDate),
+                StartPeriod = 1,
+                Duration = new TimeSpan(0, Convert.ToInt32(ExamTime), 0),
+                Status_ = 0
+            };
+            MessageBox.Show(StartDate);
+            DataProvider.Ins.DB.USINGEXAMs.Add(usingExam);
+            DataProvider.Ins.DB.SaveChanges();
+
+            // add to display
+            ListUsingExam.Add(usingExam);
+        }
+
+        private void EditExam() {
+            SelectedExam.LecturerID = SelectedSupervisor.LecturerID;
+            SelectedExam.ClassID = SelectedClass.ClassID;
+            SelectedExam.Population_ = Convert.ToInt32(Population);
+            DataProvider.Ins.DB.SaveChanges();
+        }
+
+        private void EditUsingExam()
+        {
+            SelectedUsingExam.RoomID = SelectedRoom.RoomID;
+            SelectedUsingExam.Date_ = Convert.ToDateTime(StartDate);
+            SelectedUsingExam.Duration = new TimeSpan(0, Convert.ToInt32(ExamTime), 0);
+            SelectedUsingExam.Description_ = Description;
+            DataProvider.Ins.DB.SaveChanges();
+
+            var temp = SelectedUsingExam;
+            int length = ListUsingExam.Count();
+            for (int i = 0; i < length; i++)
+            {
+                if (ListUsingExam[i].UsingExamID == SelectedUsingExam.UsingExamID)
+                {
+                    ListUsingExam.RemoveAt(i);
+                    ListUsingExam.Insert(i, temp);
+                    break;
+                }
+            }
+
+            SelectedUsingExam = temp;
+        }
+
+        private bool IsValidInput()
+        {
+            bool flag = true;
+
+            if (string.IsNullOrWhiteSpace(UsingExamID)) {
+                flag = false;
+            }
+            if (string.IsNullOrWhiteSpace(Population)) {
+                flag = false;
+            }
+            if (string.IsNullOrWhiteSpace(ExamTime)) {
+                flag = false;
+            }
+            if(SelectedClass == null) {
+                flag = false;
+            }
+            if (SelectedRoom == null) {
+                flag = false;
+            }
+            if(SelectedSupervisor == null) {
+                flag = false;
+            }
+            return flag;
+        }
+
+        private bool IsNotDuplicatedForAdding()
+        {
+            bool flag = true;
+            var classID = DataProvider.Ins.DB.CLASSes.Where(x => x.ClassID == SelectedClass.ClassID);
+
+            if (classID.Count() != 0)
+            {
+                MessageBox.Show("Lớp đã được xếp phòng thi.");
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        private bool IsNotDuplicatedForEditing()
+        {
+            bool flag = false;
+
+            if(SelectedRoom.RoomID != SelectedUsingExam.RoomID) {
+                flag = true;
+            }
+            if(SelectedClass.ClassID != SelectedExam.ClassID) {
+                flag = true;
+            }
+            if(SelectedSupervisor.LecturerID != SelectedExam.LecturerID) {
+                flag = true;
+            }
+            if(Population != SelectedExam.Population_.ToString()) {
+                flag = true;
+            }
+            if(StartDate != SelectedUsingExam.Date_.ToString()) {
+                flag = true;
+            }
+            if(ExamTime != SelectedUsingExam.Duration.ToString()) {
+                flag = true;
+            }
+            if(Description != SelectedUsingExam.Description_) {
+                flag = true;
+            }
+
+            return flag;
         }
         #endregion
 
         #region ICommand
-
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand ApproveCommand { get; set; }
+        public ICommand RejectCommand { get; set; }
+        public ICommand ConfirmCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+        public ICommand UsingExam_SelectionChangedCommand { get; set; }
         #endregion
 
         public ExamInputViewModel()
         {
             LoadData();
 
-            
+            ResetAll();
+
+            AddCommand = new RelayCommand((p) => {
+                EnableOnlyAdding();
+
+                EnableElements();
+                ResetElements();
+                SelectedUsingExam = null;
+
+                // generate id
+                UsingExamID = AutoGenerateUsingExamID();
+            });
+
+            EditCommand = new RelayCommand((p) => {
+                EnableOnlyEditing();
+                EnableElements();
+
+                UsingExamID = SelectedUsingExam.UsingExamID;
+            });
+
+            ConfirmCommand = new RelayCommand((p) => {
+                // xong het nho lam check dk
+                if(IsBeingInTask) { 
+                    if(IsAddingEnabled) {
+                        if(IsValidInput() && IsNotDuplicatedForAdding()) {
+                            AddExam();
+                            AddUsingExam();
+                            ResetAll();
+                        }
+                    }
+                    else if(IsEditingEnabled) {
+                        if(IsValidInput() && IsNotDuplicatedForEditing()) {
+                            EditExam();
+                            EditUsingExam();
+                            ResetAll();
+                        }
+                    }
+                    else if(IsDeletingEnabled) { 
+                        
+                    }
+                }
+
+                //if (IsBeingInTask && IsEditingEnabled)
+                //{
+                //    if (IsValidInput() && IsNotDuplicatedForEditing())
+                //    {
+                //        EditRoom();
+                //        ResetAll();
+                //    }
+                //}
+                //if (IsBeingInTask && IsDeletingEnabled)
+                //{
+                //    if (ListSelectedRoom.Count() == 1)
+                //    {
+                //        DeleteRoom();
+                //        ResetAll();
+                //    }
+                //    else
+                //    {
+                //        DeleteRooms();
+                //        ResetAll();
+                //    }
+                //}
+            });
+
+            CancelCommand = new RelayCommand((p) => {
+                ResetAll();
+            });
+
+            UsingExam_SelectionChangedCommand = new RelayCommand((p) =>
+            {
+                if (!(p is DataGrid)) {
+                    MessageBox.Show("ZZZ");
+                    return;
+                }
+
+                var dataGrid = p as DataGrid;
+                //GetDataGridSelectedItems(dataGrid);
+
+                if (SelectedUsingExam != null)
+                {
+                    SelectedExam = DataProvider.Ins.DB.EXAMs.Where(x => x.ExamID == SelectedUsingExam.ExamID).SingleOrDefault();
+                    SetValueForElements();
+                    IsEditingEnabled = IsDeletingEnabled = true;
+                }
+            });
         }
     }
 }
