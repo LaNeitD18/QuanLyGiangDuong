6cc2f7da76857d63095f9c61387e26500afdc026
hcthcm290 <dragonnica123@gmail.com>
2020-12-23 02:01:32 +0700

    Make timetable in homepage work

--- QuanLyGiangDuong/ViewModel/HomePage_ViewModel.cs
+++ QuanLyGiangDuong/ViewModel/HomePage_ViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using QuanLyGiangDuong.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +33,23 @@ namespace QuanLyGiangDuong.ViewModel
         }
 
         public DispatcherTimer _timer;
+
+        private ObservableCollection<table> _tb;
+        public ObservableCollection<table> tb
+        {
+            get
+            {
+                if (_tb == null)
+                {
+                    _tb = new ObservableCollection<table>();
+                    GetTimeTable();
+                }
+
+                return _tb;
+            }
+
+            set { _tb = value; OnPropertyChanged("tb"); }
+        }
         #endregion
 
         #region Functions
@@ -52,6 +71,138 @@ namespace QuanLyGiangDuong.ViewModel
         private void InitTaiKhoan()
         {
             
+        }
+
+        bool IsExistRoom(string RoomID)
+        {
+            foreach (var row in tb)
+            {
+                if (row.roomID == RoomID)
+                    return true;
+            }
+            return false;
+        }
+
+        table GetRoom(string RoomID)
+        {
+            foreach (var row in tb)
+            {
+                if (row.roomID == RoomID)
+                    return row;
+            }
+            return null;
+        }
+
+        void GetUsingClass()
+        {
+            DateTime targetDate = DateTime.Now;
+
+            // Select all needed info from USINGCLASS that fit selected Date
+            var data = (from u in DataProvider.Ins.DB.USINGCLASSes
+                        join c in DataProvider.Ins.DB.CLASSes on u.ClassID equals c.ClassID
+                        where u.Status_ == (int)Enums.UsingStatus.Approved &&
+                              targetDate >= u.StartDate && targetDate <= u.EndDate &&
+                              (int)targetDate.DayOfWeek == u.Day_ && // satisfied the day of week
+                              (DbFunctions.DiffDays(targetDate, c.StartDate) / 7) % u.RepeatCycle == 0 // satisfied the repeat cycle
+                        select new { u.UsingClassID, u.RoomID, c.ClassName, u.StartPeriod, u.Duration }).ToList();
+
+            data.Sort((a, b) => { return string.Compare(a.RoomID, b.RoomID); });
+
+            foreach (var item in data)
+            {
+                if (IsExistRoom(item.RoomID))
+                {
+                    var room = GetRoom(item.RoomID);
+                    room.Add(item.UsingClassID, item.ClassName, item.StartPeriod, item.Duration);
+                }
+                else
+                {
+                    var room = new table();
+                    room.Add(item.UsingClassID, item.ClassName, item.StartPeriod, item.Duration);
+                    room.roomID = item.RoomID;
+                    tb.Add(room);
+                }
+            }
+        }
+
+        void GetUsingEvent()
+        {
+            DateTime targetDate = DateTime.Now;
+
+            // Select all needed info from USINGCLASS that fit selected Date
+            var data = (from u in DataProvider.Ins.DB.USINGEVENTs
+                        join e in DataProvider.Ins.DB.EVENT_ on u.EventID equals e.EventID
+                        where u.Date_ == targetDate &&
+                              u.Status_ == (int)Enums.UsingStatus.Approved
+                        select new { u.UsingEventID, u.RoomID, e.EventName, u.StartPeriod, u.Duration }).ToList();
+
+            data.Sort((a, b) => { return string.Compare(a.RoomID, b.RoomID); });
+
+            foreach (var item in data)
+            {
+                if (IsExistRoom(item.RoomID))
+                {
+                    var room = GetRoom(item.RoomID);
+                    room.Add(item.UsingEventID, item.EventName, item.StartPeriod, item.Duration);
+                }
+                else
+                {
+                    var room = new table();
+                    room.Add(item.UsingEventID, item.EventName, item.StartPeriod, item.Duration);
+                    room.roomID = item.RoomID;
+                    tb.Add(room);
+                }
+            }
+        }
+
+        void GetUsingExam()
+        {
+            DateTime targetDate = DateTime.Now;
+
+            // Select all needed info from USINGCLASS that fit selected Date
+            var data = (from u in DataProvider.Ins.DB.USINGEXAMs
+                        join e in DataProvider.Ins.DB.EXAMs on u.ExamID equals e.ExamID
+                        join c in DataProvider.Ins.DB.CLASSes on e.ClassID equals c.ClassID
+                        where u.Date_ == targetDate &&
+                              u.Status_ == (int)Enums.UsingStatus.Approved
+                        select new { u.UsingExamID, u.RoomID, c.ClassName, u.StartPeriod, u.Duration }).ToList();
+
+            data.Sort((a, b) => { return string.Compare(a.RoomID, b.RoomID); });
+
+            foreach (var item in data)
+            {
+                if (IsExistRoom(item.RoomID))
+                {
+                    var room = GetRoom(item.RoomID);
+                    room.Add(item.UsingExamID, "Thi: " + item.ClassName, item.StartPeriod, item.Duration);
+                }
+                else
+                {
+                    var room = new table();
+                    room.Add(item.UsingExamID, "Thi: " + item.ClassName, item.StartPeriod, item.Duration);
+                    room.roomID = item.RoomID;
+                    tb.Add(room);
+                }
+            }
+        }
+
+        public void GetTimeTable()
+        {
+            DateTime targetDate;
+            try
+            {
+                targetDate = DateTime.Now;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Invalid Date");
+            }
+
+            tb.Clear();
+
+            GetUsingClass();
+            GetUsingEvent();
+            GetUsingExam();
         }
         #endregion
 
